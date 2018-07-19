5ba2c04587962c2b9076cde37ad7990fde7729ca
bcronin2 <bcronin2@gmail.com>
2018-07-19 16:42:54 -0700

    Set up client to retrieve data from server (#7)
    * Configure GET request for listing data in client
    
    * Update circleci config to run mongo
    
    * Add comments for reporting steps in circleci config
    
    * Remove workflows from circleci config
    
    * Begin writing test for App.jsx
    
    * Change circleci config to seed database prior to testing
    
    * Postpone testing of App.jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   },
   rules: {
     'no-underscore-dangle': 'off',
+    'react/destructuring-assignment': 'never',
   },
 };
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 
-export default function(props) {
-  return <div>Hello, world!</div>;
+const apiBaseUrl = '/api/listings';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listingData: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getDataForListing();
+  }
+
+  getDataForListing(listingId = 1) {
+    axios.get(`${apiBaseUrl}/${listingId}`).then((response) => {
+      this.setState({ listingData: response.data });
+    });
+  }
+
+  // PLACEHOLDER RENDER FUNCTION
+  render() {
+    const { title, host } = this.state.listingData;
+    if (title) {
+      return (
+        <div>
+          {host.name}
+        </div>
+      );
+    }
+    return (
+      <div>
+Hello, world!
+      </div>
+    );
+  }
 }
