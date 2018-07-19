b8863fda5ee734b93f3e2a48bef6c2df488e8bb9
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-18 20:14:23 -0700

    Initialize basic React component

--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
 import './styles/main.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('container'));
