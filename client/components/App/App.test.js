7173fce4829a8003be561db8944973140d054728
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-20 12:41:39 -0700

    Complete MVP tests for App.jsx

--- client/components/App/App.test.js
+++ client/components/App/App.test.js
@@ -6,7 +6,19 @@ import sinon from 'sinon';
 import App from './App';
 
 const mockResponse = { data: { title: 'Sample Listing' } };
-const getListingData = sinon.spy(App.prototype, 'getListingData');
+const getRequest = Promise.resolve(mockResponse);
+let getListingData;
+let axiosGet;
+
+beforeAll(() => {
+  getListingData = sinon.spy(App.prototype, 'getListingData');
+  axiosGet = sinon.stub(axios, 'get').callsFake(() => getRequest);
+});
+
+afterAll(() => {
+  getListingData.restore();
+  axiosGet.restore();
+});
 
 describe('componentDidMount()', () => {
   it('should call getDataForListing once when mounting', () => {
@@ -16,30 +28,40 @@ describe('componentDidMount()', () => {
     });
   });
   it('should receive listingData upon mounting', () => {
-    const getRequest = Promise.resolve(mockResponse);
-    const axiosGet = sinon.stub(axios, 'get').callsFake(() => getRequest);
     const wrapper = shallow(<App />);
     return Promise.resolve(wrapper).then(() => {
       expect(wrapper.state()).toHaveProperty('listingData', mockResponse.data);
-      axiosGet.restore();
     });
   });
 });
 
 describe('setListing()', () => {
   let wrapper;
+  const spy = sinon.spy(App.prototype, 'setState');
+
   beforeEach(() => {
     wrapper = mount(<App />);
   });
 
-  it('should call getDataForListing once if listingId is valid', () => {
-    getListingData.resetHistory();
+  it('should call setState once if listingId is valid', () => {
+    spy.resetHistory();
     wrapper.instance().setListing(5);
-    expect(getListingData.callCount).toBe(1);
+    expect(spy.callCount).toBe(1);
   });
-  it('should not call getDataForListing once if listingId is invalid', () => {
-    getListingData.resetHistory();
+  it('should not call setState if listingId is invalid', () => {
+    spy.resetHistory();
     wrapper.instance().setListing(-1);
-    expect(getListingData.callCount).toBe(0);
+    expect(spy.callCount).toBe(0);
+  });
+});
+
+describe('render()', () => {
+  it('should change what is displayed once component is mounted', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div')).toHaveLength(1);
+    return Promise.resolve(wrapper).then(() => {
+      wrapper.update();
+      expect(wrapper.find('div')).not.toHaveLength(1);
+    });
   });
 });
