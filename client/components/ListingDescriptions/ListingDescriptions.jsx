7c94f416f0f2d8901561b1109107d3faf2f74839
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-20 17:05:50 -0700

    Fix description.main value in dataGenerator

--- client/components/ListingDescriptions/ListingDescriptions.jsx
+++ client/components/ListingDescriptions/ListingDescriptions.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-require('./ListingSummary.scss');
+require('./ListingDescriptions.scss');
 
 export default class ListingDescriptions extends React.Component {
   constructor(props) {
     super(props);
+    this.toggleMoreInfo = this.toggleMoreInfo.bind(this);
     this.state = {
-      expanded: false
-    }
+      expanded: false,
+    };
   }
 
   toggleMoreInfo() {
@@ -15,43 +16,33 @@ export default class ListingDescriptions extends React.Component {
   }
 
   render() {
-    const { descriptions } = this.props;
+    const { main, more } = this.props.descriptions;
     const { expanded } = this.state;
-    <ListingDescription description={descriptions[0]} showName={false} />
-    <div className="moreInfo">
-
-    </div>
-    <div className="expandHide">{expanded ? "Hide" : "Read more about the space"}</div>
+    return (
+      <div>
+        <ListingDescription value={main} />
+        <div className={`moreInfo ${expanded ? '' : 'hidden'}`}>
+          {more.map(info => <ListingDescription title={info.title} value={info.value} />)}
+        </div>
+        <div className="expandHide" onClick={this.toggleMoreInfo}>
+          {expanded ? 'Hide ↑' : 'Read more about the space ↓'}
+        </div>
+      </div>
+    );
   }
+}
 
-    <div className="listingSummary">
-      <div className="listingType">
-        {listingType.value.toLowerCase()}
-      </div>
-      <div className="headline">
-        <div>
-          <div className="title">
-            {title}
-          </div>
-          <div className="city">
-            {location.city}
-          </div>
+const ListingDescription = (props) => {
+  const { title, value } = props;
+  return (
+    <div className="description">
+      {title ? (
+        <div className="title">
+          {title}
         </div>
-        <div className="host">
-          <img src={host.avatar} className="avatar" />
-          <span>
-            {host.name}
-          </span>
-        </div>
-      </div>
-      <div className="capacity">
-        {capacity.map(field => (
-          <span>
-            {field.name}
-            :
-            {field.value}
-          </span>
-        ))}
+      ) : ''}
+      <div className="value">
+        {value}
       </div>
     </div>
   );
