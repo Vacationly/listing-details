64b60a7b4ec5e3aad0a5f735a54021c3864d1e34
Ben Cronin <benc@bens-mbp.lan>
2018-07-22 13:22:16 -0700

    Clean up spacing

--- client/components/Utilities/Section/Section.jsx
+++ client/components/Utilities/Section/Section.jsx
@@ -34,7 +34,6 @@ const Section = (props) => {
             role="link"
           >
             {link}
-            {'   '}
             {expandable && (
               <img
                 className={styles.icon}
