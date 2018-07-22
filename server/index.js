6ea4a18fd678d271150d06dfa11b300665e8e527
Ben Cronin <benc@bens-mbp.lan>
2018-07-22 12:13:52 -0700

    Configure server to handle PUT request for highlight feedback

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const parser = require('body-parser');
 
 const model = require('./model.js');
 
@@ -6,10 +7,21 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use('/listings/:id', express.static(`${__dirname}/../public`));
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
 
-app.get('/api/listings/:id', (req, res) => {
-  const listingId = req.params.id;
+app.get('/api/listings/:listingId', (req, res) => {
+  const { listingId } = req.params;
   model.getListingDetails(listingId, (err, results) => {
+    res.statusCode = err ? 400 : 200;
+    res.send(err || results);
+  });
+});
+
+app.put('/api/listings/:listingId/highlights/:highlightId', (req, res) => {
+  const { listingId, highlightId } = req.params;
+  const { feedback } = req.body;
+  model.updateHighlightFeedback(listingId, highlightId, feedback, (err, results) => {
     res.statusCode = err ? 400 : 200;
     res.send(err || results);
   });
