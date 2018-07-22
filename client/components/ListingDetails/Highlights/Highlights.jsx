8c406ed9782c340aa4fde84520ebef1a9bb331c1
Ben Cronin <benc@bens-mbp.lan>
2018-07-22 12:02:17 -0700

    Refactor Highlights to use Section component

--- client/components/ListingDetails/Highlights/Highlights.jsx
+++ client/components/ListingDetails/Highlights/Highlights.jsx
@@ -1,40 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Section from '../../Utilities/Section/Section';
 import { baseUrl } from '../../../utils';
 import styles from './Highlights.css';
 
 const thumbsUpEmpty = `${baseUrl}/thumbs_up_empty.png`;
 const thumbsUpFull = `${baseUrl}/thumbs_up_full.png`;
 
-const handleUpvoteHover = (e) => {
-  const target = e.target.children[0] || e.target;
-  const oldSource = target.src;
-  target.src = oldSource === thumbsUpEmpty ? thumbsUpFull : thumbsUpEmpty;
-};
-
 export default class Highlights extends React.Component {
   constructor(props) {
     super(props);
     this.handleFeedback = this.handleFeedback.bind(this);
     this.state = {
-      feedback: [],
+      feedbackStatus: [],
     };
   }
 
   handleFeedback(index, value) {
-    const { feedback } = this.state;
-    feedback[index] = value;
+    const { feedbackStatus } = this.state;
+    feedbackStatus[index] = value;
     this.props.saveFeedback(index, value);
-    this.setState({ feedback }, () => {
+    this.setState({ feedbackStatus }, () => {
       this.props.saveFeedback(index, value);
     });
   }
 
   render() {
     const { highlights } = this.props;
-    const { feedback } = this.state;
-    return (
+    const { feedbackStatus } = this.state;
+    const content = (
       <div className={styles.highlights}>
         <div className={styles.heading}>
 home highlights
@@ -52,47 +47,64 @@ home highlights
                 {highlight.description}
               </span>
             </div>
-            <div className={styles.feedback}>
-              {feedback[highlight.id] ? (
-                <span>
-Thank you for your feedback.
-                </span>
-              ) : (
-                <span>
-                  <span
-                    className={styles.upvote}
-                    onMouseEnter={handleUpvoteHover}
-                    onMouseLeave={handleUpvoteHover}
-                    onClick={() => this.handleFeedback(highlight.id, 1)}
-                    onKeyUp={() => this.handleFeedback(highlight.id, 1)}
-                    tabIndex="0"
-                    role="link"
-                  >
-                    Helpful
-                    {' '}
-                    <img className={styles.thumbsUp} src={thumbsUpEmpty} alt="thumbs up" />
-                  </span>
-                  {' '}
-                  ·
-                  {' '}
-                  <span
-                    className={styles.downvote}
-                    onClick={() => this.handleFeedback(highlight.id, -1)}
-                    onKeyUp={() => this.handleFeedback(highlight.id, -1)}
-                    tabIndex="0"
-                    role="link"
-                  >
-                    Not helpful
-                  </span>
-                </span>
-              )}
-            </div>
+            <HighlightFeedback
+              id={highlight.id}
+              feedbackStatus={feedbackStatus}
+              handleFeedback={this.handleFeedback}
+            />
           </div>
         ))}
       </div>
     );
+    return <Section content={content} />;
   }
 }
+
+const HighlightFeedback = (props) => {
+  const { id, feedbackStatus, handleFeedback } = props;
+  const handleUpvoteHover = (e) => {
+    const target = e.target.children[0] || e.target;
+    const oldSource = target.src;
+    target.src = oldSource === thumbsUpEmpty ? thumbsUpFull : thumbsUpEmpty;
+  };
+  return (
+    <div className={styles.feedback}>
+      {feedbackStatus[id] ? (
+        <span>
+Thank you for your feedback.
+        </span>
+      ) : (
+        <span>
+          <span
+            className={styles.upvote}
+            onMouseEnter={handleUpvoteHover}
+            onMouseLeave={handleUpvoteHover}
+            onClick={() => handleFeedback(id, 1)}
+            onKeyUp={e => e.Key === 'Enter' && handleFeedback(id, 1)}
+            tabIndex="0"
+            role="link"
+          >
+            Helpful
+            {' '}
+            <img className={styles.thumbsUp} src={thumbsUpEmpty} alt="thumbs up" />
+          </span>
+          {' '}
+          ·
+          {' '}
+          <span
+            className={styles.downvote}
+            onClick={() => handleFeedback(id, -1)}
+            onKeyUp={e => e.Key === 'Enter' && handleFeedback(id, -1)}
+            tabIndex="0"
+            role="link"
+          >
+            Not helpful
+          </span>
+        </span>
+      )}
+    </div>
+  );
+};
 
 Highlights.propTypes = {
   highlights: PropTypes.arrayOf(
@@ -105,4 +117,10 @@ Highlights.propTypes = {
     }),
   ).isRequired,
   saveFeedback: PropTypes.func.isRequired,
+};
+
+HighlightFeedback.propTypes = {
+  id: PropTypes.number.isRequired,
+  feedbackStatus: PropTypes.arrayOf(PropTypes.number).isRequired,
+  handleFeedback: PropTypes.func.isRequired,
 };
