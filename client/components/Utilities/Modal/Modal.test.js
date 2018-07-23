974dbb536b06c1b090a4fd9f1cb85a2ab13d90d0
Ben Cronin <benc@bens-mbp.lan>
2018-07-22 22:28:04 -0700

    Test Modal for invalid propTypes

--- client/components/Utilities/Modal/Modal.test.js
+++ client/components/Utilities/Modal/Modal.test.js
@@ -59,41 +59,9 @@ describe('PropType errors', () => {
       return true;
     },
   };
-  const clickEvent = {
-    stopPropagation() {
-      return true;
-    },
-  };
-  const dismiss = sinon.spy(props, 'dismiss');
-
-  afterEach(() => {
-    dismiss.resetHistory();
-  });
-
-  it('should call dismiss once when clicking the x in the top left corner', () => {
-    const wrapper = shallow(<Modal {...props} />);
-    const target = wrapper.find('#dismiss');
-    target.className = 'dismiss';
-    clickEvent.target = target;
-    target.simulate('click', clickEvent);
-    expect(dismiss.callCount).toBe(1);
-  });
-
-  it('should call dismiss once when clicking the backdrop', () => {
-    const wrapper = shallow(<Modal {...props} />);
-    const target = wrapper.find('#backdrop');
-    target.className = 'backdrop';
-    clickEvent.target = target;
-    target.simulate('click', clickEvent);
-    expect(dismiss.callCount).toBe(1);
-  });
-
-  it('should call dismiss zero times when clicking the frame', () => {
-    const wrapper = shallow(<Modal {...props} />);
-    const target = wrapper.find('#frame');
-    target.className = 'frame';
-    clickEvent.target = target;
-    target.simulate('click', clickEvent);
-    expect(dismiss.callCount).toBe(0);
+  const error = sinon.spy(console, 'error');
+  it('should throw an error when given a prop of invalid type', () => {
+    shallow(<Modal {...props} />);
+    expect(error.callCount).toBe(1);
   });
 });
