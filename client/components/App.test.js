63c235c68d320bf0e4d590db5ded4bb411591cc3
Ben Cronin <benc@bens-mbp.lan>
2018-07-25 14:52:50 -0700

    Fix paths to 'utils' in tests

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import { shallow } from 'enzyme';
-import sinon from 'sinon';
-import App from './App';
-import { constants } from '../utils';
+import { shallow } from "enzyme";
+import sinon from "sinon";
+import App from "./App";
+import { constants } from "./utils";
 
 const mockResponse = { data: constants.dummyListing };
 const { dummyListing } = constants;
 
-describe('componentDidMount() with successful API request', () => {
+describe("componentDidMount() with successful API request", () => {
   const getRequest = Promise.resolve(mockResponse);
   let axiosGet;
   beforeEach(() => {
-    axiosGet = sinon.stub(axios, 'get').callsFake(() => getRequest);
+    axiosGet = sinon.stub(axios, "get").callsFake(() => getRequest);
   });
   afterEach(() => {
     axiosGet.restore();
   });
-  it('should call getDataForListing once when mounting', (done) => {
-    const getListingData = sinon.spy(App.prototype, 'getListingData');
+  it("should call getDataForListing once when mounting", done => {
+    const getListingData = sinon.spy(App.prototype, "getListingData");
     const wrapper = shallow(<App />);
     return Promise.resolve(wrapper).then(() => {
       expect(getListingData.callCount).toBe(1);
@@ -27,60 +27,60 @@ describe('componentDidMount() with successful API request', () => {
       done();
     });
   });
-  it('should receive listingData upon mounting', (done) => {
+  it("should receive listingData upon mounting", done => {
     const wrapper = shallow(<App />);
     return Promise.resolve(wrapper).then(() => {
-      expect(wrapper.state()).toHaveProperty('listingData', mockResponse.data);
+      expect(wrapper.state()).toHaveProperty("listingData", mockResponse.data);
       done();
     });
   });
-  it('should change what is displayed once component is mounted', (done) => {
+  it("should change what is displayed once component is mounted", done => {
     const wrapper = shallow(<App />);
-    const initialDiv = expect(wrapper.find('div'));
+    const initialDiv = expect(wrapper.find("div"));
     return Promise.resolve(wrapper).then(() => {
       wrapper.update();
-      expect(wrapper.find('div')).not.toEqual(initialDiv);
+      expect(wrapper.find("div")).not.toEqual(initialDiv);
       done();
     });
   });
 });
 
-describe('componentDidMount() with unsuccessful API request', () => {
+describe("componentDidMount() with unsuccessful API request", () => {
   const getRequest = Promise.reject(new Error());
   let axiosGet;
   beforeEach(() => {
-    axiosGet = sinon.stub(axios, 'get').callsFake(() => getRequest);
+    axiosGet = sinon.stub(axios, "get").callsFake(() => getRequest);
   });
   afterEach(() => {
     axiosGet.restore();
   });
-  it('should set state using "dummyListing" when server returns error', (done) => {
+  it('should set state using "dummyListing" when server returns error', done => {
     const wrapper = shallow(<App />);
     return Promise.resolve(wrapper).then(() => {
-      expect(wrapper.state()).toHaveProperty('listingData', dummyListing);
+      expect(wrapper.state()).toHaveProperty("listingData", dummyListing);
       done();
     });
   });
-  it('should change what is displayed once component is mounted', (done) => {
+  it("should change what is displayed once component is mounted", done => {
     const wrapper = shallow(<App />);
-    const initialDiv = expect(wrapper.find('div'));
+    const initialDiv = expect(wrapper.find("div"));
     return Promise.resolve(wrapper).then(() => {
       wrapper.update();
-      expect(wrapper.find('div')).not.toEqual(initialDiv);
+      expect(wrapper.find("div")).not.toEqual(initialDiv);
       done();
     });
   });
 });
 
-describe('saveFeedbackData()', () => {
+describe("saveFeedbackData()", () => {
   let axiosPut;
   beforeEach(() => {
-    axiosPut = sinon.spy(axios, 'put');
+    axiosPut = sinon.spy(axios, "put");
   });
   afterEach(() => {
     axiosPut.restore();
   });
-  it('should call axios.put to update feedback data', (done) => {
+  it("should call axios.put to update feedback data", done => {
     const wrapper = shallow(<App />);
     return Promise.resolve(wrapper).then(() => {
       wrapper.instance().saveFeedbackData();
