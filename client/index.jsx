8434cf872cb701607a322dc027ef1981edf76368
Ben Cronin <benc@bens-mbp.lan>
2018-07-25 15:44:36 -0700

    Make getting of liistingID from url more robust

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-const listingId = window.location.pathname.split('listing/')[1] || 0;
+const listingParam = window.location.pathname.split('listing/')[1];
+const listingId = parseInt(listingParam, 10) || 0;
 const props = { match: { params: { listingId } } };
 
 ReactDOM.render(<App {...props} />, window.document.getElementById('Details'));
