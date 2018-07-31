fd0512b2fb642891a65299d3449f898fe840fc97
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-07-31 10:42:53 -0700

    Make database an env var

--- data/index.js
+++ data/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const schemas = require('./schemas.js');
 
-mongoose.connect('mongodb://localhost/airbnh');
+const URI = process.env.MONGO_URI || 'mongodb://localhost/airbnh';
+mongoose.connect(URI);
 
 const listingSchema = mongoose.Schema(schemas.listing);
 const listingTypeSchema = mongoose.Schema(schemas.listingType);
