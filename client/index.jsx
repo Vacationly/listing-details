90e390570ebc49f7ba821e80c558855109a7487c
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-08-04 15:08:15 -0700

    Make react-dom a prod dependency

--- client/index.jsx
+++ client/index.jsx
@@ -1,3 +1,11 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
 import App from './components/App';
 
 window.Details = App;
+
+const container = document.getElementById('Details');
+if (container) {
+  ReactDOM.render(<App />, container);
+}
