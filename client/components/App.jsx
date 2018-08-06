dd65fa3a74eab9b045eb71fb57cfa6afb5ce25da
Ben Cronin <benc@bens-mbp.lan>
2018-08-05 17:47:30 -0700

    Reinsert componentDidMount logic for CSR page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -17,6 +17,8 @@ import { constants } from '../utils';
 
 const { apiEndpoint, dummyListing } = constants;
 
+const getListingIdFromUrl = () => parseInt(window.location.pathname.split('listing/')[1], 10) || 0;
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -25,6 +27,22 @@ export default class App extends React.Component {
       listingData: listing || dummyListing,
     };
     this.saveFeedbackData = this.saveFeedbackData.bind(this);
+  }
+
+  componentDidMount() {
+    const listingId = getListingIdFromUrl();
+    this.getListingData(listingId);
+  }
+
+  getListingData(listingId) {
+    axios.get(`${apiEndpoint}/${listingId}`).then(
+      (response) => {
+        this.setState({ listingData: response.data });
+      },
+      () => {
+        this.setState({ listingData: dummyListing });
+      },
+    );
   }
 
   saveFeedbackData(id, value) {
