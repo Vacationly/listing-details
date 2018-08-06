8bdacefc2287d1b8cebf67a167fb19c41151b41b
Ben Cronin <benc@bens-mbp.lan>
2018-08-05 22:54:12 -0700

    Remove extra comments

--- server/html.js
+++ server/html.js
@@ -1,9 +1,3 @@
-/**
- * Html
- * This Html.js file acts as a template that we insert all our generated
- * application code into before sending it to the client as regular HTML.
- * Note we're returning a template string from this function.
- */
 const Html = ({ body, title, styles }) => `
   <!DOCTYPE html>
   <html>
